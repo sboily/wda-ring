1484188faaeafdd2cb0e7a8cdefa0f46bb1b5153
Sylvain Boily <sylvain@wazo.io>
2023-04-17 16:39:32 -0400

    Make menu dynamic

--- ring.js
+++ ring.js
@@ -3,15 +3,42 @@ import i18next from 'https://cdn.jsdelivr.net/gh/i18next/i18next/src/index.js';
 
 const ringElem = document.getElementById("ring");
 
+const options = {
+  "original": "Original",
+  "iphone.wav": "Iphone",
+  "landline1.wav": "Landline 1",
+  "landline2.wav": "Landline 2",
+  "marimba.wav": "Marimba",
+  "ring3.wav": "Ring 3",
+  "ring4.wav": "Ring 4",
+  "ring5.wav": "Ring 5",
+  "dark_side_ringtone.mp3": "Stars Wars"
+};
+
 app.onIframeMessage = (msg) => {
-  const ring = msg.ring.split("/").pop();
-  ringElem.value = ring;
+  if (msg.ring) {
+    const ring = msg.ring.split("/").pop();
+    ringElem.value = ring;
+  }
+}
+
+const addOptionMenu = (options, idMenu) => {
+  const menu = document.getElementById(idMenu);
+
+  for (const option in options) {
+    const newOption = document.createElement("option");
+    newOption.text = options[option];
+    newOption.value = option;
+    menu.add(newOption);
+  }
 }
 
 (async() => {
   await app.initialize();
   const context = app.getContext();
   const lang = context.app.locale;
+
+  addOptionMenu(options, "ring");
 
   app.sendMessageToBackground({value: 'config'});
   ring.addEventListener("change", function() {
