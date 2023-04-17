386ccb9af125138f5349af0f275586b7ec0bfdf3
Sylvain Boily <sylvain@wazo.io>
2023-04-17 16:26:21 -0400

    add translation support

--- ring.js
+++ ring.js
@@ -1,4 +1,5 @@
 import app from 'https://cdn.jsdelivr.net/npm/@wazo/euc-plugins-sdk@latest/lib/esm/app.js';
+import i18next from 'https://cdn.jsdelivr.net/gh/i18next/i18next/src/index.js';
 
 const ringElem = document.getElementById("ring");
 
@@ -9,9 +10,32 @@ app.onIframeMessage = (msg) => {
 
 (async() => {
   await app.initialize();
+  const context = app.getContext();
+  const lang = context.app.locale;
+
   app.sendMessageToBackground({value: 'config'});
   ring.addEventListener("change", function() {
     const ring = ringElem.value;
     app.sendMessageToBackground({value: 'ring', data: ring});
   });
+
+  await i18next.init({
+    lng: lang,
+    fallbackLng: 'en',
+    debug: false,
+    resources: {
+      en: {
+        translation: {
+          "choose_ring": "Select your ringtone"
+        }
+      },
+      fr: {
+        translation: {
+          "choose_ring": "Choisissez votre sonnerie"
+        }
+      }
+    }
+  });
+
+  document.getElementById('choose-ring').innerHTML = i18next.t('choose_ring');
 })();
